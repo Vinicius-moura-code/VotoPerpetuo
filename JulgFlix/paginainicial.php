aac75a07a053ca61f1b83d866e93ca144a61d227
Bruna Nunes da Silva <brunans.email@gmail.com>
2021-05-06 21:03:50 -0300

    JulgFlix

---
+++ JulgFlix/paginainicial.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title>JulgFlix</title>
+  <link rel="icon" href="logo2.ico">
+  <link href="estilo.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+  <div id="cabecalho">
+  <img id="logo" src="logo.png"></br>
+  <u id="menu">
+    <a id="menu2" href="usuario.php">Usuário</a> 
+    <a id="menu2" href="filmes.php">Filme</a> 
+    <a id="menu2" href="series.php">Séries</a>
+    <a id="menu2" href="animes.php">Animes</a>
+    <a id="menu2" href="desenhos.php">Desenhos</a>
+  </u>
+  </div>
+
+  <div id="conteudo">
+  <p> cadastre quantos filmes, série, anime ou desenhos quiser e deixe as pessoas julgarem seus gostos.</p>
+  </div>
+
+  <footer id="rodape">Made with 💕 by Bruna Silva</footer>
+</body>
+</html>
