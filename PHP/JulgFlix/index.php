599ba2e865c3393891a1152b41416c9e2558a267
Vinicius-moura-code <viniciusrebelo487@gmail.com>
2021-05-06 23:30:32 -0300

    Insert File

---
+++ PHP/JulgFlix/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <title>JulgFlix</title>
+  <link rel="icon" href="assets/logo2.ico">
+  <!-- CSS only -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+  <link rel="stylesheet" href="./styles/style.css">
+
+</head>
+
+<body>
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-2">
+      </div>
+      <div id="logo" class="col-md-8">
+        <img src="assets/logo.png"></br>
+
+      </div>
+      <div class="col-md-2">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <nav class="navbar navbar-inverse">
+
+
+          <ul id="menu" class="nav navbar">
+            <li><a href="pages/usuario.php">Usuário</a></li>
+            <li><a href="pages/filmes.php">Filme</a></li>
+            <li><a href="pages/series.php">Séries</a></li>
+            <li><a href="pages/animes.php">Animes</a></li>
+            <li><a href="pages/desenhos.php">Desenhos</a></li>
+          </ul>
+
+        </nav>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-2">
+      </div>
+      <div id="conteudo" class="col-md-8">
+        <h3 class="text-center">
+          JulgFlix, cadastre quantos filmes, série, anime ou desenhos quiser e deixe as pessoas julgarem seus gostos. </h3>
+      </div>
+      <div class="col-md-2">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <div class="footer">
+        Made with ❤ by Bruna Silva
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- JavaScript Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
+</body>
+
+</html>
