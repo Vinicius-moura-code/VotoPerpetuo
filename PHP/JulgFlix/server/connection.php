1620560de5c3d7a018be96488e4b83465152cb64
Bruna Nunes da Silva <silvabruna.email@gmail.com>
2021-06-14 20:50:18 -0300

    julgflix

--- PHP/JulgFlix/server/connection.php
+++ PHP/JulgFlix/server/connection.php
@@ -2,7 +2,7 @@
 
 define("DB_HOST", "localhost:3306");
 define("DB_USER","root");
-define("DB_PASSWORD", "");
+define("DB_PASSWORD", "toor");
 define("DB_DATABASE", "jugflix");
 $connection=mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_DATABASE);
 ?>
