96b7f8f15ce86f2a5bdb41e70b3f29af875c9488
Vinicius Moura <viniciusrebelo487@gmail.com>
2022-08-05 00:52:46 -0300

    weee

--- Saidas/index.js
+++ Saidas/index.js
@@ -1,9 +1,33 @@
-const botao = document.getElementsByClassName("botao");
-let locais = ['Bomba Burguer Canoas', 'Bomba Burguer Sapucaia', 'Bah Restaurante', 'Coco Bambu', 'Outback',
-              'Jardim do lago', 'Tomattis', 'Italianissimo', 'Manos', 'Miski'];
+const botao = document.getElementsByClassName('botao');
+const resposta = document.getElementById('resposta');
 
+const display = (s) => (resposta.innerText = s);
+
+let locais = [
+  'Bomba Burguer Canoas',
+  'Bomba Burguer Sapucaia',
+  'Bah Restaurante',
+  'Coco Bambu',
+  'Outback',
+  'Jardim do lago',
+  'Tomattis',
+  'Italianissimo',
+  'Manos',
+  'Miski',
+];
+
+const delayLoop = (fn, delay) => {
+  return (locais, i) => {
+    setTimeout(() => {
+      display(locais);
+    }, i * 1000);
+  };
+};
 
 function gerarlocal() {
-    let x = (Math.random() * locais.length);
-    alert(locais[x]);
+  let x = Math.floor(Math.random() * locais.length) + 1;
+  if (x === locais.length) x -= 1;
+  let nums = ['🎵🎵🎵', '🎵🎵', '🎵'];
+  nums.push(locais[x]);
+  nums.forEach(delayLoop(display, 1000));
 }
