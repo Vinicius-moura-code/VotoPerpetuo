77b686a4ba623b01cc2d343fa1194234ab6746a1
eiBrunaSilva <silvabruna.email@gmail.com>
2022-08-18 13:07:46 -0300

    atualizacao

--- Saidas/index.js
+++ Saidas/index.js
@@ -1,6 +1,6 @@
 const botao = document.getElementsByClassName("botao");
 let locais = ['Bomba Burguer Canoas', 'Bomba Burguer Sapucaia', 'Bah Restaurante', 'Coco Bambu', 'Outback',
-              'Jardim do lago', 'Tomattis', 'Italianissimo', 'Manos', 'Miski'];
+              'Jardim do lago', 'Tomattis', 'Italianissimo', 'Manos', 'Miski', 'Caldeirão Furado Pub'];
 
 
 function gerarlocal() {
